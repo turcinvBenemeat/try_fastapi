de5632669d1ac8c9a40d2b4267ab1ecff1c303bf
Vit Turcin <turcinv@btlnet.com>
2026-03-23 12:30:39 +0100

    Testing

--- main.py
+++ main.py
@@ -2,7 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+def index():
+    return {"Hello": "World"}
+
+@app.get("/hi")
+def greet():
+    return f"Hello? World?"
+
+# URL Path
+@app.get("/hi/{name}")
+def greet(name: str):
+    return f"Hello? {name}?"
+
+# Query Parameters
+@app.get("/hello")
+def hello(name: str):
+    return f"Hello, {name}?"
+
+@app.get("/ping")
+def ping():
+    return {"ping": "pong"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
